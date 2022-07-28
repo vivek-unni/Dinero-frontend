4c7fa1269af30547ad4f40ce9f6e666c7cbf9b3e
Tomin Joy <tominjk007@gmail.com>
2022-07-28 18:06:44 +0530

    📎

---
+++ assets/js/auth.js
@@ -0,0 +1,28 @@
+function auth(){
+    console.log("started")
+    var name=document.getElementById('email').value;
+    var pass=document.getElementById('password').value; 
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+       if (this.readyState == 4 && this.status == 200) {
+        if(this.response){
+            var res = this.responseText.split("<br>");
+            if(res[1]){
+                document.cookie=`token = ${res[1]}`;
+                location.replace("./assets/html/Dashboard-user.html");
+            }else{
+                document.getElementById('pass-er').style.display="block";
+                setTimeout(()=>{document.getElementById('pass-er').style.display="none";},3000);
+            }
+        }else{
+            document.getElementById('usr-er').style.display="block";
+            setTimeout(()=>{document.getElementById('usr-er').style.display="none";},3000);
+        }
+       }
+     };
+    xhttp.open("POST", "https://dinero-server.herokuapp.com/auth.php", true);
+    // xhttp.open("POST","http://localhost/Server/auth.php",true)
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send(`fname=${name}&pass=${pass}`);
+ }
+ document.getElementById('submit').addEventListener("click",()=>{auth()})
