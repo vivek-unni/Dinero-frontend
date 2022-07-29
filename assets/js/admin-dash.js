ee0306eab5ea26d683db61590b3a26162f3b9e2d
Tomin Joy <tominjk007@gmail.com>
2022-07-29 12:31:56 +0530

    hi hi

--- assets/js/admin-dash.js
+++ assets/js/admin-dash.js
@@ -13,4 +13,3 @@ function logout(){
     document.cookie = 'token=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
     location.replace("./");
 }
-document.getElementById('logout').addEventListener("click",logout);
